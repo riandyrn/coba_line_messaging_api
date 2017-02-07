7663329190c4e315072779d62616e0fb5997a7db
Riandy Rahman Nugraha <riandyrn@gmail.com>
2017-02-07 22:23:06 +0700

    coba pake line-messaging library

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var app = require('express')(),
     http = require('http').Server(app),
+    LINEBot = require('line-messaging'),
+    bot = LINEBot.create({
+        channelID: '1500022422',
+        channelSecret: '287963ddd21a67efcb379af8e70e0308',
+        channelToken: '3RtR8DgOh+fmIeFxJo1DqQjizXKG1WwtZke8TXkSKO91hjenf52LzkwsS+bAyXZsqsgVNh+rCDwAlzBUV1qmKTu3/6ZUimPu5f+6pJdfr6IcKI6Wl20TJAXKRiZA1Q5759kcqFToFzboACRSkZo4IAdB04t89/1O/w1cDnyilFU='
+    }, http),
     port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.get('/', function(req, res){
     
-    res.send('<h1>Hello World!</h1>');
+    res.send('<h1>Hello my name is Riandy Rahman Nugraha!</h1>');
 });
 
-http.listen(port, function(){
+app.use(bot.webhook('/webhook'));
+
+bot.on(LINEBot.Events.MESSAGE, function(replyToken, message){
     
-    console.log('listening on port ' + port);
+    console.log("replyToken: " + replyToken);
+    console.log("message: " + message);
 });
+
+http.listen(port);
