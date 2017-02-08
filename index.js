06ddb07b47cf278f4072ff2208aac241d2eef8b5
Riandy Rahman Nugraha <riandyrn@gmail.com>
2017-02-08 07:19:59 +0700

    change to simple logger

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 var app = require('express')(),
-    http = require('http').Server(app),
-    LINEBot = require('line-messaging'),
-    bot = LINEBot.create({
-        channelID: '1500022422',
-        channelSecret: '287963ddd21a67efcb379af8e70e0308',
-        channelToken: '3RtR8DgOh+fmIeFxJo1DqQjizXKG1WwtZke8TXkSKO91hjenf52LzkwsS+bAyXZsqsgVNh+rCDwAlzBUV1qmKTu3/6ZUimPu5f+6pJdfr6IcKI6Wl20TJAXKRiZA1Q5759kcqFToFzboACRSkZo4IAdB04t89/1O/w1cDnyilFU='
-    }, http),
+    server = require('http').Server(app),
+    bodyParser = require('body-parser'),
     port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     
     res.send('<h1>Hello my name is Riandy Rahman Nugraha!</h1>');
 });
 
-app.use(bot.webhook('/webhook'));
-
-bot.on(LINEBot.Events.MESSAGE, function(replyToken, message){
+app.post('/', function(req, res){
     
-    console.log("replyToken: " + JSON.stringify(replyToken));
-    console.log("message: " + JSON.stringify(message));
+    console.log(req.headers);
+    console.log(req.body);
+    
+    res.status(200).json({status: "success"});
 });
 
-http.listen(port);
+server.listen(port);
